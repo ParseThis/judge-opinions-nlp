c8f4c5fc7c87872cb5e53bcd307ef3d25c506502
orson <orson.network@gmail.com>
2020-01-14 01:29:00 -0500

    clean up load_data

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,48 @@
 
+import numpy as np
 import pandas as pd
+from cleaner import Cleaner
+
+
+
+def collect_all_data(cls, df):
+
+    judges = df.copy()
+    #
+    for plaintext in judges.plain_text:
+
+       top_split, bottom_split =  Cleaner.splitter(plaintext)
+       bad_names = Cleaner._unclean_names(top_split)
+       good_names = Cleaner._clean_names(bad_names)
+       author = Cleaner._clean_author(bottom_split)
+
+       yield list(good_names), author
+
+
+def build_judges(cls, df):
+    #build placeholder datframe
+    jd = pd.DataFrame(index = range(len(df)),
+                      columns=['Judge 1', 'Judge 2', 'Judge 3', 'Author'])
+    #collect all names
+    names = cls.collect_all_data(df)
+    namer =[]
+    auths = []
+    for _names, auth  in  names:
+         namer.append(_names)
+         auths.append(auth)
+    #pack placeholder with real data dataframe
+    for i, name in enumerate(namer):
+        try: #try upacking name s
+            j1, j2, j3 = name
+        except ValueError:
+            j1, j2, j3 = [np.nan]*3
+        finally:
+            jd.loc[i] = j1, j2, j3, i
+    jd['Author'] = auths; jd.index = df.index
+    #join Dataframes
+    return  df.join(jd)
+
+
 
 
 def import_json(files):
@@ -23,9 +66,8 @@ def import_json(files):
 if __name__ == '__main__':
 
 	party = pd.read_csv('./jb.txt')
-	for key in party.keys():
-	    if 'Party' in key:
-	        print key
+	df = import_json()
+
 
 	party_aff = [col_name for col_name in party.keys() if 'Party' in col_name]
 	party_col = party.loc[:, party_aff]
@@ -36,10 +78,8 @@ if __name__ == '__main__':
 	party['Party Affiliation of President (2)'].notnull()]\
 	[['Judge Last Name', 'Judge First Name', 'Birth Year']]
 
-	df = import_json()
-
 	df = df[df.plain_text != u'']
-	opinions = BuildDataFrame.build_judges(df)
+	opinions = build_judges(df)
 	opinions = opinions.dropna(subset = ['Judge 1'])
 	opinions.loc[0:, ['Judge 3']]  = [re.sub(r',' ,'', x) for x in opinions['Judge 3']]
 	opinions.Author = list(Cleaner.a_c(opinions))
@@ -60,9 +100,6 @@ if __name__ == '__main__':
 	    ['Party Affiliation of President'] == 'Republican',1, 0)
 
 	g =  opinions.loc[0:,['party_1', 'party_2', 'party_3']].sum(1).tolist()
-
 	f = {'1' : 3, '2': 2, '3' : 1, '0' : 4}
-
 	panels = map(lambda x: f[str(x)], g)
-
 	opinions['panel_type'] = panels 
